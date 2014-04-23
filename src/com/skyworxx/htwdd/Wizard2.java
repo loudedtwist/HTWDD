19c38fcc813d241fd6f4d0b9ce04fb3719bb16fb
Kay Förster <red.mail2@gmail.com>
2014-04-23 21:46:59 +0200

    #1 Aktualisierung des Impressums

--- src/com/skyworxx/htwdd/Wizard2.java
+++ src/com/skyworxx/htwdd/Wizard2.java
@@ -12,14 +12,11 @@ import android.widget.Button;
 
 public class Wizard2 extends Fragment
 {
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.wizard2, null);
     }
-
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState)
@@ -27,33 +24,14 @@ public class Wizard2 extends Fragment
         super.onActivityCreated(savedInstanceState);
 
         Button b1 = (Button) getActivity().findViewById(R.id.button1);
-        Button b2 = (Button) getActivity().findViewById(R.id.button2);
-
 
         b1.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View arg0)
             {
-
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VYWUBBQ6T6PT2"));
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/HTWDD/HTWDD"));
                 startActivity(browserIntent);
-
             }
         });
-
-        b2.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View arg0)
-            {
-
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.Mark-Schramm.de"));
-                startActivity(browserIntent);
-
-            }
-        });
-
-
     }
-
-
 }
