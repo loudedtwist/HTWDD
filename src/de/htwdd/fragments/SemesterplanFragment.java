49bfb3ced78f92506897a9163add374bed4d3489
Kay Förster <red.mail2@gmail.com>
2014-05-11 16:23:45 +0200

    Update Semesterplan / Studienangelegenheiten

--- src/de/htwdd/fragments/SemesterplanFragment.java
+++ src/de/htwdd/fragments/SemesterplanFragment.java
@@ -15,20 +15,14 @@ import de.htwdd.R;
 
 public class SemesterplanFragment extends Fragment
 {
-
-
     public SemesterplanFragment()
     {
-
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
         return inflater.inflate(R.layout.semesterplan, null);
-
     }
 
 
@@ -36,65 +30,5 @@ public class SemesterplanFragment extends Fragment
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
-
-
     }
-
-
-    private class worker extends AsyncTask<Object, Integer, String[]>
-    {
-
-
-        @Override
-        protected String[] doInBackground(Object... params)
-        {
-//			try {
-//				Thread.sleep(2000);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-
-            HTTPDownloader downloader = new HTTPDownloader("http://www2.htw-dresden.de/~rawa/cgi-bin/auf/sem_plan.php");
-
-
-            String result = downloader.getsafeString();
-
-            String semester = "";
-
-            if (result.contains("Wintersemester")) semester = "Wintersemester";
-            if (result.contains("Sommersemester")) semester = "Sommersemester";
-
-            String[] semesterplan = {semester};
-
-
-            return semesterplan;
-
-        }
-
-        protected void onProgressUpdate(Integer... progress)
-        {
-
-
-        }
-
-
-        @Override
-        protected void onPostExecute(String[] essen)
-        {
-            try
-            {
-                TextView semester = (TextView) getActivity().findViewById(R.id.semester);
-
-                semester.setText(essen[0]);
-
-            } catch (Exception e)
-            {
-
-            }
-        }
-
-    }
-
-
 }
