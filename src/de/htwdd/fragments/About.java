e4d3405fec9a222e19c071819103b8af5664bd20
Kay Förster <red.mail2@gmail.com>
2014-12-02 17:02:24 +0100

    App-Info: Neugestaltung, Kontaktmöglichkeiten ergänzt

--- src/de/htwdd/fragments/About.java
+++ src/de/htwdd/fragments/About.java
@@ -1,5 +1,7 @@
 package de.htwdd.fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import de.htwdd.R;
 
@@ -17,7 +20,7 @@ public class About extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.about, null);
+        return inflater.inflate(R.layout.about, container, false);
     }
 
     @Override
@@ -25,15 +28,50 @@ public class About extends Fragment
     {
         super.onActivityCreated(savedInstanceState);
 
-        Button b1 = (Button) getActivity().findViewById(R.id.button1);
+        final Activity activity = getActivity();
 
-        b1.setOnClickListener(new View.OnClickListener()
+        TextView about_email = (TextView) activity.findViewById(R.id.about_email);
+        about_email.setOnClickListener(new View.OnClickListener()
         {
-            public void onClick(View arg0)
+            @Override
+            public void onClick(View v)
             {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/HTWDD/HTWDD"));
+                sendEmail(activity,new String[]{getText(R.string.about_email).toString()},"Write Email to HTWDresden App", "[Android]", "");
+            }
+        });
+
+
+        TextView about_website = (TextView) activity.findViewById(R.id.about_link_website);
+        about_website.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(activity.getResources().getString(R.string.about_link_website)));
+                startActivity(browserIntent);
+            }
+        });
+
+        TextView about_github = (TextView) activity.findViewById(R.id.about_link_github);
+        about_github.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(activity.getResources().getString(R.string.about_link_github)));
                 startActivity(browserIntent);
             }
         });
     }
+
+    public static void sendEmail(Context context, String[] recipientList, String title, String subject, String body)
+    {
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, recipientList);
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+        context.startActivity(Intent.createChooser(emailIntent, title));
+    }
+
 }
