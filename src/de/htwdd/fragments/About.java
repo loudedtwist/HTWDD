8ff34189699f6ea3ac40fc7bd27aedff484bf5cc
Kay Förster <red.mail2@gmail.com>
2015-02-27 18:02:31 +0100

    Verlorene Änderungen

--- src/de/htwdd/fragments/About.java
+++ src/de/htwdd/fragments/About.java
@@ -9,7 +9,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import de.htwdd.R;
@@ -20,7 +19,7 @@ public class About extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.about, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
     @Override
